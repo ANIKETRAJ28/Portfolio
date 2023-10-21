641041aa9c14f85c152b7e78f50413748624a6ee
Aniket <aniketraj28042003@gmail.com>
2023-10-21 18:28:55 +0530

    addedhome page

---
+++ script.js
@@ -0,0 +1,63 @@
+let profileSkill = document.querySelector(".profile h2");
+let resume = document.querySelector("#resume");
+function profileAnimation() {
+  let tl = gsap.timeline();
+  let profileName = document.querySelector(".profile h1");
+  tl.from(profileName, {
+    y: 100,
+    duration: 0.5,
+    delay: 0.3,
+  });
+  tl.from(profileSkill, {
+    y: 100,
+  });
+  let navtl = gsap.timeline();
+  let tabs = document.querySelectorAll(".nav-tabs span");
+  tabs.forEach((tab) => {
+    navtl.from(tab, {
+      y: -100,
+      duration: 0.5,
+      opacity: 0,
+    });
+  });
+  let soctl = gsap.timeline();
+  let socials = document.querySelectorAll(".nav-socials a");
+  socials.forEach((s) => {
+    soctl.from(s, {
+      y: -100,
+      duration: 0.5,
+      opacity: 0,
+    });
+  });
+}
+profileAnimation();
+
+let themeSwitch = document.querySelector("#mode");
+let flag;
+themeSwitch.addEventListener("click", () => {
+  if (!flag) {
+    document.querySelector(".profile-container").style.backgroundColor =
+      "#000000";
+    document.querySelector(".profile-container").style.color = "#ffffff";
+    resume.style.color = "#000000";
+    resume.style.backgroundColor = "#ffffff";
+    profileSkill.style.color = "#000000";
+    profileSkill.style.backgroundColor = "#ffffff";
+    document.querySelector(".nav-container").style.backgroundColor = "#000000";
+    document.querySelector(".nav-tabs").style.color = "#ffffff";
+    themeSwitch.style.color = "#ffffff";
+    flag = true;
+  } else {
+    document.querySelector(".profile-container").style.color = "#000000";
+    document.querySelector(".profile-container").style.backgroundColor =
+      "#ffffff";
+    resume.style.color = "#ffffff";
+    resume.style.backgroundColor = "#000000";
+    profileSkill.style.color = "#ffffff";
+    profileSkill.style.backgroundColor = "#000000";
+    document.querySelector(".nav-container").style.backgroundColor = "#ffffff";
+    document.querySelector(".nav-tabs").style.color = "#000000";
+    themeSwitch.style.color = "#000000";
+    flag = false;
+  }
+});
