ea967321d91b7c470c1894fed5a96db4b2b0e20b
Aniket <aniketraj28042003@gmail.com>
2023-10-24 11:00:23 +0530

    added codes

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function profileAnimation() {
     delay: 0.5,
   });
   let navtl = gsap.timeline();
-  navtl.from(" .nav-socials a, .nav-socials div", {
+  navtl.from(" .nav-socials a", {
     y: -100,
     duration: 1,
     delay: 0.2,
@@ -28,18 +28,23 @@ function profileAnimation() {
   let soctl = gsap.timeline();
   soctl.from("#articles,#projects, #about, #home", {
     y: -100,
-    duration: 1,
-    delay: 0.2,
+    duration: 0.6,
+    delay: -0.8,
     opacity: 0,
-    stagger: 0.3,
+    stagger: 0.2,
   });
 }
 profileAnimation();
 
 function cross_animation() {
+  let flag = true;
   let cross = document.querySelector(".crossanimation");
+  let idx = 0;
+  let newidx;
   cross.addEventListener("click", () => {
     cross.classList.toggle("cross-animation");
+    let nav_tabs_spans = document.querySelector(".nav-tabs-spans");
+    nav_tabs_spans.classList.toggle("active");
   });
 }
 
